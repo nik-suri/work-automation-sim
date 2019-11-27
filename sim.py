caaf057805bf237c4c2ab93d9663e5e9755057a1
Nikhil Suri <nikhilsuri@comcast.net>
2019-11-26 23:31:34 -0800

    fixed excel outputs

--- sim.py
+++ sim.py
@@ -21,7 +21,13 @@ for i in msa_employment_df.index:
     occ_code = msa_employment_df['OCC_CODE'][i]
     current_job_employment = msa_employment_df['TOT_EMP'][i]
     if occ_code in automation_p and current_job_employment != "**":
-        economy_model[occ_code] = [current_job_employment, 0]
+        job_title = msa_employment_df['OCC_TITLE'][i]
+        economy_model[occ_code] = {
+            'employed': current_job_employment,
+            'automated': 0,
+            'title': job_title
+        }
+
 
 # find keys in automation_p not in economy_model
 delete_keys = []
@@ -40,11 +46,16 @@ auto_rate = 0.2
 
 for t in range(time_steps):
     for job in automation_p.keys():
-        curr_econ_size, curr_automated = economy_model[job]
+        job_data = economy_model[job]
+        curr_econ_size, curr_automated = job_data['employed'], job_data['automated']
+
         new_econ_size = round((1 + growth_rate) * curr_econ_size)
         automated_conversion = round(automation_p[job] * auto_rate * new_econ_size)
         new_automated = curr_automated + automated_conversion
-        economy_model[job] = [new_econ_size - automated_conversion, new_automated]
+
+        job_data['employed'] = new_econ_size - automated_conversion
+        job_data['automated'] = new_automated
+
     print("time step " + str(t) + " completed")
 
 economy_df = pd.DataFrame(economy_model).T
