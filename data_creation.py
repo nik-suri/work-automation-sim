238eb8d896f48fd7f0274c1b573b005e755854e7
Nikhil Suri <nikhilsuri@comcast.net>
2019-11-28 13:05:21 -0800

    aggregating granular projections into metropolitan areas

--- data_creation.py
+++ data_creation.py
@@ -2,22 +2,43 @@ import sys
 import pandas as pd
 from openpyxl import load_workbook, Workbook
 
-def main():
-    clean_regional_file('oak$OccProj.xlsx')
-    clean_regional_file('sanf$OccProj.xlsx')
-    clean_regional_file('sanrf$OccProj.xlsx')
+RAW_REGIONAL_PREFIX = 'raw_data_files/alltb6/'
+CLEAN_REGIONAL_PREFIX = 'clean_data_files/cleaned_'
 
-# clean employment data
-# msa_employment_df = pd.read_excel('raw_data_files/oesm18ma/MSA_M2018_dl.xlsx', sheet_name='MSA_dl')
-# msa_filtered = msa_employment_df.query('AREA_NAME == "San Francisco-Oakland-Hayward, CA" and TOT_EMP != "**"')[['OCC_CODE', 'TOT_EMP', 'OCC_TITLE']]
-# msa_filtered.to_excel('clean_data_files/sf_employment.xlsx', sheet_name='Sheet1')
-# print(msa_filtered)
-# print("employment data written to sf_employment.xlsx")
+def main():
+    clean_regional_proj_file('oak$OccProj.xlsx')
+    clean_regional_proj_file('sanf$OccProj.xlsx')
+    clean_regional_proj_file('sanrf$OccProj.xlsx')
+
+    aggregate_proj('oak$OccProj.xlsx', 'sanf$OccProj.xlsx', 'sanrf$OccProj.xlsx')
+
+    # clean employment data
+    msa_employment_df = pd.read_excel('raw_data_files/oesm18ma/MSA_M2018_dl.xlsx', sheet_name='MSA_dl')
+    msa_filtered = msa_employment_df.query('AREA_NAME == "San Francisco-Oakland-Hayward, CA" and TOT_EMP != "**"')[['OCC_CODE', 'TOT_EMP', 'OCC_TITLE']]
+    msa_filtered.to_excel('clean_data_files/sf_employment.xlsx', sheet_name='Sheet1')
+    print(msa_filtered)
+    print("employment data written to sf_employment.xlsx")
+
+
+# aggregate granular county projections into a mean metropolitan projection
+def aggregate_proj(*args):
+    aggregate_df = pd.read_excel(CLEAN_REGIONAL_PREFIX + args[0])
+    for i in range(1, len(args)):
+        to_add_df = pd.read_excel(CLEAN_REGIONAL_PREFIX + args[i])
+        aggregate_df = aggregate_df.merge(to_add_df, on='SOC_CODE')
+        aggregate_df['ANNUAL_CHANGE'] = aggregate_df['ANNUAL_CHANGE_x'] + aggregate_df['ANNUAL_CHANGE_y']
+        del aggregate_df['ANNUAL_CHANGE_x']
+        del aggregate_df['ANNUAL_CHANGE_y']
+
+    aggregate_df['ANNUAL_MEAN_CHANGE'] = aggregate_df['ANNUAL_CHANGE'].div(len(args))
+    del aggregate_df['ANNUAL_CHANGE']
+    aggregate_df.to_excel('clean_data_files/sf_employment_projections.xlsx', sheet_name='Sheet1')
+
 
 # clean local projection data excel files
-def clean_regional_file(filename):
-    raw_filename = 'raw_data_files/alltb6/' + filename
-    clean_filename = 'clean_data_files/cleaned_' + filename
+def clean_regional_proj_file(filename):
+    raw_filename = RAW_REGIONAL_PREFIX + filename
+    clean_filename = CLEAN_REGIONAL_PREFIX + filename
 
     raw_wb = load_workbook(filename=raw_filename)
     clean_wb = Workbook()
@@ -28,19 +49,19 @@ def clean_regional_file(filename):
     for i in range(4, raw_ws.max_row):
         for j in range(1, raw_ws.max_column):
             clean_ws.cell(row=i - 3,column=j).value = raw_ws.cell(row=i,column=j).value
+
     clean_ws.delete_cols(1, 1)
     clean_ws.delete_cols(2, 4)
     clean_ws.delete_cols(3, 8)
     clean_ws.delete_rows(2, 1)
 
     # should delete all rows which are meaningless at the end
+    empty_rows = 18
+    clean_ws.delete_rows(clean_ws.max_row - empty_rows, empty_rows + 1)
 
     for row, cellObj in enumerate(list(clean_ws.columns)[1]):
         if row != 0:
-            try:
-                cellObj.value = (float(cellObj.value) + 1) ** 0.1 - 1
-            except TypeError:
-                continue
+            cellObj.value = (float(cellObj.value) + 1) ** 0.1 - 1
 
     clean_ws['A1'] = 'SOC_CODE'
     clean_ws['B1'] = 'ANNUAL_CHANGE'
@@ -48,8 +69,6 @@ def clean_regional_file(filename):
     clean_wb.save(filename=clean_filename)
     print('cleaned ' + filename)
 
-
-# oakland (alameda and contra costa)
 
 if __name__ == "__main__":
     main()
